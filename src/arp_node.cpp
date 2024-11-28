b4c0b8c9126fc8fb00b9c5e74dbf331160438497
Afonso Baldo <up2004598@edu.fe.up.pt>
2024-11-28 11:25:38 +0000

    Task 5 done!
    Co-authored-by: Ahmad Traboulsi <atraboulsi27@users.noreply.github.com>
    Co-authored-by: Ricardo <ricayanzon@users.noreply.github.com>

--- src/arp_node.cpp
+++ src/arp_node.cpp
@@ -6,6 +6,8 @@
 #include <SDL2/SDL.h>
 
 #include <ros/ros.h>
+#include <ros/package.h>
+
 #include <image_transport/image_transport.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
@@ -21,18 +23,26 @@
 #include <arp/cameras/PinholeCamera.hpp>
 #include <arp/cameras/RadialTangentialDistortion.hpp>
 
+#include <arp/ViEkf.hpp>
+#include <arp/VisualInertialTracker.hpp>
+#include <arp/Frontend.hpp>
+#include <arp/StatePublisher.hpp>
+
 class Subscriber
 {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  arp::VisualInertialTracker* tracker;
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
+    if (!tracker) return;
     uint64_t timeMicroseconds = uint64_t(msg->header.stamp.sec) * 1000000ll
         + msg->header.stamp.nsec / 1000;
     // -- for later use
     std::lock_guard<std::mutex> l(imageMutex_);
     lastImage_ = cv_bridge::toCvShare(msg, "bgr8")->image;
+    tracker->addImage(timeMicroseconds, lastImage_);
   }
 
   bool getLastImage(cv::Mat& image)
@@ -47,18 +57,26 @@ class Subscriber
 
   void imuCallback(const sensor_msgs::ImuConstPtr& msg)
   {
-    // -- for later use
+    if (!tracker) return;
+    uint64_t timeMicroseconds = uint64_t(msg->header.stamp.sec) * 1000000ll
+        + msg->header.stamp.nsec / 1000;
+    Eigen::Vector3d acc(msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
+    Eigen::Vector3d angular(msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z);
+    tracker->addImuMeasurement(timeMicroseconds, acc, angular);
+
+    
   }
+
 
  private:
   cv::Mat lastImage_;
   std::mutex imageMutex_;
+  
 };
 
-/// @brief inicialize camera with camera parameters
+/// @brief get camera parameters
 /// @return true on success
-arp::cameras::PinholeCamera<arp::cameras::RadialTangentialDistortion> initCamera(ros::NodeHandle& nh){
-  double fu=0, fv=0, cu=0, cv=0, k1=0, k2=0, p1=0, p2=0;
+bool getCameraParameters(ros::NodeHandle& nh, double& fu, double&fv, double& cu, double& cv, double& k1, double& k2, double& p1, double&p2){
   bool success = true;
   
   auto getParam = [&nh, &success](const std::string& param_name, double& param_value) {
@@ -82,10 +100,7 @@ arp::cameras::PinholeCamera<arp::cameras::RadialTangentialDistortion> initCamera
   ROS_INFO("cu: %f, cv: %f ", cu, cv);
   ROS_INFO("k1: %f, k2: %f ", k1, k2);
   ROS_INFO("p1: %f, p2: %f ", p1, p2);
-  arp::cameras::RadialTangentialDistortion distortion(k1,k2,p1,p2);
-
-  return arp::cameras::PinholeCamera<arp::cameras::RadialTangentialDistortion>(
-        640, 360, fu, fv, cu, cv, distortion);
+  return success;
 }
 
 int main(int argc, char **argv)
@@ -118,10 +133,59 @@ int main(int argc, char **argv)
   // enter main event loop
   std::cout << "===== Hello AR Drone ====" << std::endl;
 
+
+  // read camera params
+  double fu=0, fv=0, cu=0, cv=0, k1=0, k2=0, p1=0, p2=0;
+  getCameraParameters(nh, fu, fv, cu, cv, k1, k2, p1, p2);
+
   // init camera
-  auto camera = initCamera(nh);
+  auto camera = arp::cameras::PinholeCamera<arp::cameras::RadialTangentialDistortion>(
+        640, 360, fu, fv, cu, cv, arp::cameras::RadialTangentialDistortion(k1,k2,p1,p2));
   camera.initialiseUndistortMaps();
   bool undistorted = false;
+
+  // set up frontend -- use parameters as loaded in previous practical
+  arp::Frontend frontend(640, 360, fu, fv, cu, cv, k1, k2, p1, p2);
+
+  // load map
+  std::string path = ros::package::getPath("ardrone_practicals");
+  std::string mapFile;
+  if(!nh.getParam("arp_node/map", mapFile))
+    ROS_FATAL("error loading parameter");
+  std::string mapPath = path+"/maps/"+mapFile;
+  if(!frontend.loadMap(mapPath))
+    ROS_FATAL_STREAM("could not load map from " << mapPath << " !");
+  
+  // state publisher -- provided for rviz visualisation of drone pose:
+  arp::StatePublisher pubState(nh);
+
+  // set up EKF
+  arp::ViEkf viEkf;
+  Eigen::Matrix4d T_SC_mat;
+  std::vector<double> T_SC_array;
+  if(!nh.getParam("arp_node/T_SC", T_SC_array))
+    ROS_FATAL("error loading parameter");
+  T_SC_mat <<
+    T_SC_array[0], T_SC_array[1], T_SC_array[2], T_SC_array[3],
+    T_SC_array[4], T_SC_array[5], T_SC_array[6], T_SC_array[7],
+    T_SC_array[8], T_SC_array[9], T_SC_array[10], T_SC_array[11],
+    T_SC_array[12], T_SC_array[13], T_SC_array[14], T_SC_array[15];
+  arp::kinematics::Transformation T_SC(T_SC_mat);
+  viEkf.setCameraExtrinsics(T_SC);
+  viEkf.setCameraIntrinsics(frontend.camera());
+  
+  // set up visual-inertial tracking
+  arp::VisualInertialTracker visualInertialTracker;
+  visualInertialTracker.setFrontend(frontend);
+  visualInertialTracker.setEstimator(viEkf);
+  subscriber.tracker = &visualInertialTracker;
+  
+  // set up visualisation: publish poses to topic ardrone/vi_ekf_pose
+  visualInertialTracker.setVisualisationCallback(std::bind(
+        &arp::StatePublisher::publish, &pubState, std::placeholders::_1,
+        std::placeholders::_2));
+
+
   auto last_time = std::chrono::steady_clock::now();
 
   cv::Mat image;
